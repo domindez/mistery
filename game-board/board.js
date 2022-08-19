e363186ab5a4673e7e851c8082a1d5fa2c3371ed
domindez <109239601+domindez@users.noreply.github.com>
2022-08-19 19:14:48 +0200

    añadido Path

---
+++ game-board/board.js
@@ -0,0 +1,58 @@
+const { startPos, treasurePos, Path } = require("./game-config")
+
+// Board
+
+let grid = [];
+let nRows = 9;
+let nCol = 6;
+
+for (let i = 1; i < nRows + 1; i++) {
+
+  for (let j = 1; j < nCol + 1; j++) {
+    grid.push({
+
+      id: "t" + i.toString() + j.toString(),
+      row: i,
+      col: j,
+    });
+  }
+}
+
+// Set adjacent cells
+
+grid.forEach(tile => {
+  tile.adjacentCells = [
+    grid.filter(t => t.row === tile.row - 1 && t.col == tile.col)[0] != undefined ? grid.filter(t => t.row === tile.row - 1 && t.col == tile.col)[0].id : null,
+    grid.filter(t => t.row === tile.row + 1 && t.col == tile.col)[0] != undefined ? grid.filter(t => t.row === tile.row + 1 && t.col == tile.col)[0].id : null,
+    grid.filter(t => t.col === tile.col - 1 && t.row == tile.row)[0] != undefined ? grid.filter(t => t.col === tile.col - 1 && t.row == tile.row)[0].id : null,
+    grid.filter(t => t.col === tile.col + 1 && t.row == tile.row)[0] != undefined ? grid.filter(t => t.col === tile.col + 1 && t.row == tile.row)[0].id : null
+  ]
+});
+
+// Set posición inicial y botella
+
+const initialPosXY = startPos;
+let initialPos = grid.filter(t => t.id == initialPosXY)[0];
+
+const xMarkTheSpot = treasurePos
+const treasureTile = grid.filter(t => t.id == xMarkTheSpot)[0];
+treasureTile.treasue = true;
+
+// Set death tiles con lo que viene del game config
+
+grid.forEach(() => {
+  deathTiles = grid.filter(t => Path.includes(t.id) == false)
+});
+
+deathTiles.forEach(tile => {
+  tile.death = true
+});
+
+
+
+module.exports = {
+  grid : grid,
+  initialPos : initialPos,
+  treasure : treasureTile.id
+}
+
